064623c9a2fa50899c1eb77947350c16b9a28d03
heejeong1106 <heejeong1106@gmail.com>
2025-05-03 14:23:14 +0900

    Toast UI Calendar 설치 완료

---
+++ src/pages/EventPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div ref="calendarContainer" style="height: 800px;"></div>
+  </template>
+  
+  <script>
+  import Calendar from '@toast-ui/calendar';
+  import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+  
+  export default {
+    name: 'EventPage',
+    mounted() {
+      this.calendar = new Calendar(this.$refs.calendarContainer, {
+        defaultView: 'month',
+        usageStatistics: false
+      });
+    }
+  };
+  </script>
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import MainPage from "@/pages/MainPage.vue";
+import EventPage from "@/pages/EventPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [ 
     {path:'/' ,  component : MainPage } ,
+    {path:'/event' ,  component : EventPage } ,
 ];
 const router = createRouter({
     history : createWebHistory() , 
