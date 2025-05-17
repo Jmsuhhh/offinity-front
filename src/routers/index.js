1017eed6975742fe6dc1b64feaddfa6757f8f13d
Eunjin-n <ikee1652@naver.com>
2025-05-17 15:57:41 +0900

    회원가입/로그인 페이지 1차

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,14 @@
 import MainPage from "@/pages/MainPage.vue";
 import EventPage from "@/pages/EventPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
+import LogInPage from "@/pages/LogInPage.vue";
+import SignUpPage from "@/pages/SignUpPage.vue";
 
 const routes = [ 
     {path:'/' ,  component : MainPage } ,
     {path:'/event' ,  component : EventPage } ,
+    {path: '/login' , component : LogInPage} ,
+    {path: '/signup' , component : SignUpPage}
 ];
 const router = createRouter({
     history : createWebHistory() , 
