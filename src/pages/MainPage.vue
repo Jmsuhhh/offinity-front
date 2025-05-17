cb4dff64aaa502e1bbe842cc53c2f216bc3fc342
hongjt70 <hongjt70@hotmail.com>
2025-05-17 18:04:16 +0900

    결재 화면 추가 및 기존 일부 보완

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1>main page</h1>
+  <h2>Main</h2>
 </template>
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
 import MainPage from "@/pages/MainPage.vue";
 import EventPage from "@/pages/EventPage.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import LeaveHome from "@/pages/LeaveHome.vue";
+import LeaveApprovalPage from "@/pages/LeaveApprovalPage.vue";
 
 const routes = [ 
     {path:'/' ,  component : MainPage } ,
     {path:'/event' ,  component : EventPage } ,
+    {path:'/leave/:id', component : LeaveHome},
+    {path:'/leave/:id/approval', component : LeaveApprovalPage},
 ];
 const router = createRouter({
     history : createWebHistory() , 
